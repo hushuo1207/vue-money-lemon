e324907b64329ef3a1da4f1ccdf21c908524b14c
hushuo <1253740807@qq.com>
2021-12-21 17:55:57 +0800

    开始vue-lemon记账开发

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script lang="ts">
+import x from '@/components/test.vue'
+export default {
+  
+}
+</script>
