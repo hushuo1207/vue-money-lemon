b0ca12e7050931ab039c298b477fa94c50adafde
hushuo <1253740807@qq.com>
2021-12-22 14:26:49 +0800

    模块化把Money分成四个子组件

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <label class="notes">
+            <span class="name">
+                备注
+            </span>
+            <input class="value" type="text" placeholder="在此输入备注...">
+        </label>
+            
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Notes',
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+@import '~@/assets/style/helper.scss';
+.notes{
+    font-size: 14px;
+    background: #f5f5f5;
+    display: flex;
+    padding-left: 16px;
+    align-items: center;
+    .name{
+        padding-right: 16px;
+
+    }
+    input{
+        height: 64px;
+        flex-grow: 1;
+        background: transparent;
+        border: none;
+        padding-right: 16px;
+    }
+}
+</style>
---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="tags">
+            <div class="new">
+                <button>新增标签</button>
+            </div>
+            <ul class="current">
+                <li>衣</li>
+                <li>食</li>
+                <li>住</li>
+                <li>行</li>
+            </ul>
+        </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Tags',
+    }
+</script>
+
+<style lang="scss" scoped>
+@import '~@/assets/style/helper.scss';
+.tags{
+    flex-grow: 1;
+    font-size: 14px;
+    padding: 16px;
+    display: flex;
+    flex-direction: column-reverse;
+    > .current{
+        display: flex;
+        flex-wrap: wrap;
+        > li {
+            background: #d9d9d9;
+            height: 24px;
+            line-height: 24px;
+            border-radius: 12px;
+            padding: 0 16px;
+            margin-right: 12px;
+        }
+    }
+    > .new{
+        padding-top: 16px;
+        button{
+            background: transparent;
+            border: none;
+            color: #999;
+            border-bottom: 1px solid;
+            padding: 0 4px; 
+        }
+    }
+}
+</style>
