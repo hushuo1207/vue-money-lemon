3add5b55ef65a32cbe25a2ef84c7d6d1abcb361c
hushuo <1253740807@qq.com>
2021-12-22 21:13:28 +0800

    吧record的复制保存至lrecordist，连接localStorage

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,22 +1,51 @@
 <template>
     <div class="tags">
             <div class="new">
-                <button>新增标签</button>
+                <button @click="createTag">新增标签</button>
             </div>
             <ul class="current">
-                <li>衣</li>
-                <li>食</li>
-                <li>住</li>
-                <li>行</li>
+                <li v-for="tag in dataSource" :key="tag"
+                    :class="{selected: selectedTags.indexOf(tag)>=0}" @click="toggleTag(tag)">
+                    {{tag}}
+                </li>
             </ul>
         </div>
 </template>
 
 <script lang="ts">
-    export default {
-        name: 'Tags',
+import Vue from "vue";
+import {Component, Prop} from "vue-property-decorator";
+
+@Component
+export default class Tags extends Vue {
+    @Prop() readonly dataSource: string[] | undefined;
+    //为了保证在本组件下不修改其他组件传来的值
+    selectedTags: string[] = [];
+
+    toggleTag(tag: string){
+        const index = this.selectedTags.indexOf(tag);
+        if(index >= 0){
+            this.selectedTags.splice(index, 1);
+        }else {
+            this.selectedTags.push(tag);
+        }
+    }
+    createTag(){
+        const name = window.prompt('请输入标签名')
+        // console.log(name);
+        if (name === ''){
+            window.alert('标签不能为空');
+        }else if(this.dataSource){
+            console.log(this.dataSource);
+            
+            this.$emit('update:dataSource', [...this.dataSource, name]);
+            console.log(this.dataSource);
+
+        }
         
     }
+
+}
 </script>
 
 <style lang="scss" scoped>
@@ -36,7 +65,12 @@
             line-height: 24px;
             border-radius: 12px;
             padding: 0 16px;
+            margin-top: 4px;
             margin-right: 12px;
+            &.selected{
+                background: #15a19a;
+                color: white;
+            }
         }
     }
     > .new{
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,30 +3,33 @@
         <NumberPad />
         <Types  />
         <Notes />
-        <Tags />
+        <Tags :data-source.sync="tags" @xxx="yyy" />
     </Layout>
 </template>
 
 <script lang="ts">
-import NumberPad from '@/components/Money/NumberPad.vue'
-import Notes from '@/components/Money/Notes.vue'
-import Tags from '@/components/Money/Tags.vue'
-import Types from '@/components/Money/Types.vue'
- export default {
-    name: "Money",
+import Vue from 'vue';
+import NumberPad from '@/components/Money/NumberPad.vue';
+import Notes from '@/components/Money/Notes.vue';
+import Tags from '@/components/Money/Tags.vue';
+import Types from '@/components/Money/Types.vue';
+import Component from 'vue-property-decorator';
+
+@Component({
     components: { NumberPad, Notes, Tags, Types }
+})
+export default class Money extends Vue {   
+    tags = ['衣','食','住','行','hehda'];
+        
 }
 
 
 </script>
+
 <style lang="scss">
   .layout-content{
     //   border: 1px solid red;
       display: flex;
       flex-direction: column-reverse;
   }
-</style>
-
-<style lang="scss" scoped>
-
 </style>
