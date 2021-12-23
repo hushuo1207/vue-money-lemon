eca3e0664caf3b49745afab07bdfec24589bd090
hushuo <1253740807@qq.com>
2021-12-23 13:41:44 +0800

    改造Notes.vue 使其成为通用组件

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,10 +1,8 @@
 <template>
     <div>
         <label class="notes">
-            <span class="name">
-                备注
-            </span>
-            <input  type="text" placeholder="在此输入备注..."
+            <span class="name">{{this.fileName}}</span>
+            <input  type="text" :placeholder="this.placeholder"
               v-model="value">
         </label>
             
@@ -13,11 +11,14 @@
 
 <script lang="ts">
 import Vue from "vue";
-import {Component, Watch} from "vue-property-decorator";
+import {Component, Prop, Watch} from "vue-property-decorator";
 
 @Component
 export default class Notes extends Vue {
     value = '';
+    @Prop({required: true}) fileName!: string;
+    @Prop() placeholder?: string;
+
 
     @Watch('value')
     onValueChanged(value: string){
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,6 +1,10 @@
 <template>
     <Layout>
-        编辑标签
+        <div>
+            <Icon name="left" />
+            <span>编辑标签</span>
+        </div>
+        <Notes file-name="标签名" placeholder="请输入标签名" />
     </Layout>
 </template>
 
@@ -9,8 +13,11 @@
 import Vue from 'vue'
 import { Component, Prop, Watch } from 'vue-property-decorator';
 import tagListModel from '@/Models/tagListModel';
+import Notes from '../components/Money/Notes.vue';
 
-@Component
+@Component({
+    components: {Notes}
+})
 export default class EditLabel extends Vue{
     created () {
         const id = this.$route.params.id;
