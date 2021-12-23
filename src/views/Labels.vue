e312effe3b562d922aaf1d35905b76a6f75f33c3
hushuo <1253740807@qq.com>
2021-12-23 13:23:41 +0800

    添加点击标签可跳转标签详情

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,11 +1,12 @@
 <template>
     <Layout>
-        <ol class="tags">
-            <li v-for="tag in tags" :key="tag.id">
+        <div class="tags">
+            <router-link class="tag" v-for="tag in tags" :key="tag.id"
+              :to="`/labels/edit/${tag.id}`">
                 <span>{{tag.name}}</span>
                 <Icon name="right"/>
-            </li>
-        </ol>
+            </router-link>
+        </div>
         <div class="createTag-wrapper">
             <button class="createTag" @click="createTag">新建标签</button>
         </div>
@@ -42,7 +43,7 @@ tagListModel.fetch()
     background: white;
     font-size: 16px;
     padding-left: 16px;
-    > li {
+    > .tag {
        min-height: 44px;
       display: flex;
       align-items: center;
@@ -70,31 +71,5 @@ tagListModel.fetch()
     }
   }
 
-    /* > .tag {
-      min-height: 44px;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      border-bottom: 1px solid #e6e6e6;
-      svg {
-        width: 18px;
-        height: 18px;
-        color: #666;
-        margin-right: 16px;
-      }
-    }
-  }
-  .createTag {
-    background: #767676;
-    color: white;
-    border-radius: 4px;
-    border: none;
-    height: 40px;
-    padding: 0 16px;
-    &-wrapper {
-      text-align: center;
-      padding: 16px;
-      margin-top: 44-16px;
-    }
-  } */
+
 </style>
