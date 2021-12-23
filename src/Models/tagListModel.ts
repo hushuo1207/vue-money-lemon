2ce85306ebce08d8f8d126f608292b9dd7f28142
hushuo <1253740807@qq.com>
2021-12-23 11:11:37 +0800

    将tagListModel 内的tag string 改为{id name}

--- src/Models/tagListModel.ts
+++ src/Models/tagListModel.ts
@@ -1,7 +1,12 @@
 const localStorageKeyName = 'tagList';
+type Tag = {
+    id: string;
+    name: string;
+}
+
 type TagListModel = {
-    data: string[]
-    fetch: () => string[]
+    data: Tag[]
+    fetch: () => Tag[]
     create: (name: string) => 'success' |  'duplicated'
     save: () => void
 }
@@ -15,12 +20,13 @@ const tagListModel: TagListModel = {
         return this.data;
     },
     create(name){
-        if(this.data.indexOf(name) >= 0) {return 'duplicated';}
-        this.data.push(name);
+        const names = this.data.map(item => item.name);
+        if (names.indexOf(name) >= 0) {return 'duplicated';}
+        //const id = createId().toString();
+        this.data.push({id: name, name: name});
         this.save();
-
         return 'success';
-    },
+        },
     save() {
         window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
     }
---
+++ src/views/EditLabel.vue
@@ -0,0 +1,20 @@
+<template>
+    <Layout>
+        编辑标签
+    </Layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop, Watch } from 'vue-property-decorator';
+
+
+@Component
+export default class Labels extends Vue{
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
