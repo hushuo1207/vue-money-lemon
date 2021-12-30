d088276fe8b6e6bae6c3e1ce32679c683c06da25
hushuo <1253740807@qq.com>
2021-12-30 21:36:02 +0800

    添加+ - 功能，处理数据

--- src/components/Money/NumberPadTest.vue
+++ src/components/Money/NumberPadTest.vue
@@ -19,7 +19,7 @@
             <div class="button" @click="inputContent">7</div>
             <div class="button" @click="inputContent">8</div>
             <div class="button" @click="inputContent">9</div>
-            <div class="button date" @click="xyy">
+            <div class="button date" >
               <DatePicker
                 
                 placement="left-end"
@@ -32,7 +32,7 @@
                 @on-ok="handleOk">
                 <a class="abc" href="javascript:void(0)" @click="handleClick">
                     <Icon  v-if="record.createdAt === new Date().toISOString().split('T')[0]" name="day"></Icon>
-                    <template  v-if="record.createdAt === new Date().toISOString().split('T')[0]">今天</template>
+                    <template  v-if="record.createdAt === day(new Date().toISOString()).format('YYYY-MM-DD')">今天</template>
                     <template v-else>{{ record.createdAt }}</template>
                 </a>
               </DatePicker>
@@ -40,17 +40,26 @@
             <div class="button" @click="inputContent">4</div>
             <div class="button" @click="inputContent">5</div>
             <div class="button" @click="inputContent">6</div>
-            <div class="button" @click="clear">+</div>
+            <div class="button" @click="increase">
+              <Icon name="plus" />
+            </div>
             <div class="button" @click="inputContent">1</div>
             <div class="button" @click="inputContent">2</div>
             <div class="button" @click="inputContent">3</div>
-            <div class="button" @click="yyyy">-</div>
+            <div class="button" @click="decrease">
+              <Icon name="minus" />
+              
+            </div>
             <div class="button" @click="inputContent">.</div>
             <div class="button" @click="inputContent">0</div>
             <div class="button delete" @click="remove">
               <Icon name='delete' />
             </div>
-            <div class="button ok" @click="ok" >OK</div>
+            
+            <div v-if="equalSymbol" class="button ok" @click="saveRecord" >OK</div>
+            <div v-else class="button" @click="equalOutput" >
+              <Icon name="equal" />
+            </div>
         </div>  
 
       </div>
@@ -67,7 +76,7 @@ import TagsTest from '@/components/Money/TagsTest.vue';
 import TabsTest from '@/components/Money/TabsTest.vue';
 import recordTypeList from '@/constants/recordTypeList';
 const {DatePicker}= require('view-design')
-import dayjs from 'dayjs'
+import day from 'dayjs'
 
 // console.log('DatePicker');
 // console.log(DatePicker);
@@ -82,30 +91,12 @@ export default class NumberPadTest extends Vue {
           tags: [], notes: '', type: '-', amount: 0, createdAt: new Date().toISOString().split('T')[0]
     };
     recordTypeList = recordTypeList;
+    day = day;
     created(){
         this.$store.commit('fetchRecords');
         // this.$store.commit('fetchTags');
     }
-    
-    // @Prop(Number) readonly number!: number ;
-    // created(){
-        
-        
-    //     this.$store.commit('fetchRecords');
-    //     // this.$store.commit('fetchTags');
 
-    // }
-    // saveRecord(){
-    //     if(!this.record.tags || this.record.tags.length === 0){
-    //         return window.alert('请至少选择一个标签')
-    //     }
-    //     this.$store.commit('createRecord', this.record);
-    //     if(this.$store.state.createRecordError === null) {
-    //         window.alert('创建成功')
-    //         this.record.notes = '';
-    //     }
-
-        
     // } this.number.toString() ||''
     output: string = '0';
     // i: number =  0;
@@ -143,15 +134,50 @@ export default class NumberPadTest extends Vue {
     clear() {
         this.output = '0';
     }
-    yyyy(){
-      const input = document.createElement('input');
-      console.dir(input);
-      
+    equalNumber =0;
+    symbol = true;
+    equalSymbol = true;
+    increase(){
+      this.equalNumber = parseFloat(this.output);
+      this.output = '0';
+      this.symbol = true;
+      this.equalSymbol = false;
     }
-    xyy(){
+    
+    decrease(){
+      this.equalNumber = parseFloat(this.output);
+      this.output = '0';
+      this.symbol = false;
+      this.equalSymbol = false;
 
     }
-    ok () {
+    equalOutput(){
+      if(this.symbol){
+        this.output = (this.equalNumber + parseFloat(this.output)).toString();
+        
+        this.equalSymbol = true;
+
+        return;
+      }
+      const a = this.equalNumber - parseFloat(this.output)
+      
+      if(a<0){
+        alert('不支持负数哦')
+        this.output = '0';
+        
+      }else{
+        this.output = (a).toString();
+        let arr = this.output.split('.');
+        if(arr[1] && arr[1].length > 2){
+          let array2= arr[1].split('')
+          this.output = arr[0] + '.' + array2[0] + array2[1];
+        }
+      }
+      this.equalSymbol = true;
+
+      // this.output = '0';
+    }
+    saveRecord () {
       this.record.amount = parseFloat(this.output);
       if(!this.record.tags || this.record.tags.length === 0){
             return window.alert('请至少选择一个标签')
@@ -298,7 +324,7 @@ height: 50vh;
         }
         &.date{
           height: 7vh;
-          background: #fff177;
+          // background: #fff177;
           //width: 24px;
           
         }
@@ -312,7 +338,7 @@ height: 50vh;
         &:nth-child(3), &:nth-child(6), &:nth-child(9) {
           background: darken($bg, 4*2%);
         }
-        &:nth-child(7), &:nth-child(10), &:nth-child(13) {
+        &:nth-child(4), &:nth-child(7), &:nth-child(10), &:nth-child(13) {
           background: darken($bg, 4*3%);
         }
         &:nth-child(8), &:nth-child(11), &:nth-child(14) {
