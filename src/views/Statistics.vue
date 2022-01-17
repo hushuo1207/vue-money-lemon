0d968842b0792a074a8d6aa8862c420e64dd2864
hushuo <1253740807@qq.com>
2022-01-17 20:57:30 +0800

    添加capture

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout class-prefix="layout">
-    <div class="tabs-wrapper" @click="toDay">
+    <div class="tabs-wrapper" @click.capture="toDay">
       <TabsTest
         class-prefix="type"
         :data-source="recordTypeList"
